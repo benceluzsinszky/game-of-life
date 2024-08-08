6f677a91101b17ce0d7abccd42a264612e3b8107
bence <benceluzsinszky@gmail.com>
2024-08-08 17:28:58 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import GameGrid from './components/gamegrid';
+import './styles/App.css';
+
+export default function App() {
+  const [size, setSize] = useState(10);
+  const [speed, setSpeed] = useState(1);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const [gridArray, setGridArray] = useState(Array(size * size).fill(0));
+
+
+  const handleSizeChange = (event) => {
+    setSize(Number(event.target.value));
+  };
+
+  const handleSpeedChange = (event) => {
+    setSpeed(Number(event.target.value));
+  };
+
+  const handleClear = () => {
+    setIsRunning(false);
+    setGridArray(gridArray.fill(0));
+    gridArrayToCss(gridArray);
+  };
+
+  const gridArrayToCss = (gridArray) => {
+    for (let i = 0; i < gridArray.length; i++) {
+      if (gridArray[i] === 1) {
+        document.getElementById(`cell-${i}`).className = 'game-cell alive';
+      } else {
+        document.getElementById(`cell-${i}`).className = 'game-cell dead';
+      }
+    }
+  };
+
+
+  return (
+    <>
+      <header>
+        <h1>Conway&apos;s Game of Life</h1>
+      </header>
+      <main>
+        <section>
+          <GameGrid
+            size={size}
+            speed={speed}
+            isRunning={isRunning}
+            gridArray={gridArray}
+            setGridArray={setGridArray}
+            gridArrayToCss={gridArrayToCss} />
+        </section>
+        <section>
+          <h2>Controls</h2>
+          <label htmlFor="sizeSlider">Grid Size: {size} </label>
+          <input
+            id="sizeSlider"
+            type="range"
+            min="5"
+            max="100"
+            value={size}
+            onChange={handleSizeChange}
+          /><br />
+          <label htmlFor='speedSlider'>Speed: {speed} </label>
+          <input
+            id='speedSlider'
+            type='range'
+            min='1'
+            max='10'
+            value={speed}
+            onChange={handleSpeedChange}
+          /><br />
+          <button id="start" onClick={() => setIsRunning(true)}>Start</button>
+          <button id="stop" onClick={() => setIsRunning(false)}>Stop</button>
+          <button id="clear" onClick={handleClear}>Clear</button>
+        </section>
+      </main>
+    </>
+  );
+};
---
+++ src/components/gamegrid.jsx
@@ -0,0 +1,157 @@
+import { PropTypes } from 'prop-types';
+import { useCallback, useEffect } from 'react';
+import '../styles/gamegrid.css';
+
+export default function GameGrid({ size, speed, isRunning, gridArray, setGridArray, gridArrayToCss }) {
+
+
+    const createGrid = (size) => {
+        const cellSize = screen.height / 2 / size;
+
+        let rows = [];
+        for (let i = 0; i < size; i++) {
+            let cells = [];
+            for (let j = 0; j < size; j++) {
+                let index = i * size + j;
+                cells.push(<td
+                    key={index}
+                    className={`game-cell dead`}
+                    title={`${index}`}
+                    id={`cell-${index}`}
+                    style={
+                        {
+                            width: `${cellSize}px`,
+                            height: `${cellSize}px`,
+                        }
+                    }
+                    onClick={handleClickedCell}></td>);
+            }
+            rows.push(<tr key={i}>{cells}</tr>);
+        }
+        return (
+            <table className='game-grid'>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        );
+    };
+
+    const handleClickedCell = (event) => {
+        let newGridArray = [...gridArray];
+        const cellIndex = event.target.id.split('-')[1];
+
+        if (gridArray[cellIndex] === 1) {
+            newGridArray[cellIndex] = 0;
+        } else {
+            newGridArray[cellIndex] = 1;
+        }
+
+        setGridArray(newGridArray);
+        gridArrayToCss(newGridArray);
+    };
+
+    const getNumberOfNeighbors = useCallback((cellIndex) => {
+        let neighbors = 0;
+
+        // check top row
+        for (let i = cellIndex - size - 1; i <= cellIndex - size + 1; i++) {
+            if (i < 0) {
+                continue;
+            }
+            const cell = gridArray[i];
+            if (cell && cell === 1) {
+                neighbors++;
+            }
+        }
+        // check left
+        if (cellIndex % size !== 0) {
+            const previousCell = gridArray[cellIndex - 1];
+            if (previousCell && gridArray[cellIndex - 1] === 1) {
+                neighbors++;
+            }
+        }
+
+        // check right
+        if (cellIndex % size !== size - 1) {
+            const nextCell = gridArray[cellIndex + 1];
+            if (nextCell && gridArray[cellIndex + 1] === 1) {
+                neighbors++;
+            }
+        }
+
+        // check bottom row
+        for (let i = cellIndex + size - 1; i <= cellIndex + size + 1; i++) {
+            if (i > size * size) {
+                continue;
+            }
+            const cell = gridArray[i];
+            if (cell && cell === 1) {
+                neighbors++;
+            }
+        }
+        console.log(neighbors);
+        return neighbors;
+    }, [gridArray, size]);
+
+    const gameLoop = useCallback(() => {
+
+        if (!isRunning) {
+            return;
+        }
+
+        const cellsNumber = size * size;
+
+        let newGridArray = [...gridArray];
+
+        for (let i = 0; i < cellsNumber; i++) {
+            const cell = gridArray[i];
+            const neighbors = getNumberOfNeighbors(i);
+
+            if (cell === 1) {
+                if (neighbors < 2 || neighbors > 3) {
+                    newGridArray[i] = 0;
+                }
+            } else {
+                if (neighbors === 3) {
+                    newGridArray[i] = 1;
+                }
+            }
+        }
+
+        setGridArray(newGridArray);
+        gridArrayToCss(newGridArray);
+
+    }, [gridArray, setGridArray, gridArrayToCss, isRunning, size, getNumberOfNeighbors]);
+
+    const calculateGameSpeed = useCallback(() => {
+        return 200 / speed * 2;
+    }, [speed]);
+
+    useEffect(() => {
+        if (!isRunning) {
+            return;
+        }
+        const timer = setTimeout(() => {
+            gameLoop();
+        }, calculateGameSpeed());
+
+        // Cleanup the timeout on component unmount
+        return () => clearTimeout(timer);
+    }, [isRunning, gameLoop, calculateGameSpeed]);
+
+    return (
+        <>
+            {createGrid(size)}
+        </>
+    );
+};
+
+GameGrid.propTypes = {
+    size: PropTypes.number.isRequired,
+    speed: PropTypes.number.isRequired,
+    isRunning: PropTypes.bool.isRequired,
+    gridArray: PropTypes.array.isRequired,
+    setGridArray: PropTypes.func.isRequired,
+    gridArrayToCss: PropTypes.func.isRequired,
+};
